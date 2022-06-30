e3f29f67cadf89bd676bc0dd1873ca154df10c9f
Grigory3v <jack977j@gmail.com>
2022-07-01 00:16:29 +0400

    добавил task_03

---
+++ task_03/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace dz3
+{
+class Program
+{
+static void Main(string[] args)
+{
+    
+    Console.WriteLine("Введите цифру, обозначающую день недели ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if ( 1 <= number && number <= 7)
+    {
+        if ( number != 6 && number != 7 )
+        { 
+            Console.WriteLine("Этот день НЕ является выходным");
+        }
+
+        else Console.WriteLine("Этот день является выходным");
+    }
+    else Console.WriteLine(" Нет такого дня недели");
+
+}
+}
+}
