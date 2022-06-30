b6053c0100a75cbee97c4a5bec230d2bac77f679
Grigory3v <jack977j@gmail.com>
2022-07-01 00:16:10 +0400

    добавил task_02

---
+++ task_02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace task_01
+{
+class Program
+{
+static void Main(string[] args)
+{
+    Console.WriteLine("Введите число");    
+    string? str = Convert.ToString(Console.ReadLine());
+    if (str != null)
+    {
+        if (str.Length <= 2)
+        {
+        Console.WriteLine("третьей цифры нет");
+        } 
+        else Console.Write(str[2]);
+    }
+}
+}
+}
