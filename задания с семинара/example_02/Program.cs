22015b106bb2457b2250ac2bbcef096cf393be60
Grigory3v <jack977j@gmail.com>
2022-07-01 00:21:46 +0400

    добавил задания с семинара

---
+++ задания с семинара/example_02/Program.cs
@@ -0,0 +1,23 @@
+﻿   namespace task_02
+{
+class Program
+{
+static void Main(string[] args)
+{
+   
+   
+   int remainder = 0;
+    Console.WriteLine("Введите первое число");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+
+    if ( number1 % number2 == 0 ){
+        Console.WriteLine($"Число {number2} является кратным числу {number1} ");
+    }
+    else Console.WriteLine($"Число {number2} НЕявляется кратным числу {number1}, остаток {remainder = number1 % number2} ");
+
+}
+}
+}
