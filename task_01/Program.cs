7465f7f643a0d4fc7114c99392237648b0dea96b
Grigory3v <jack977j@gmail.com>
2022-07-01 00:16:03 +0400

    добавил task_01

---
+++ task_01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace task_01
+{
+class Program
+{
+static void Main(string[] args)
+{
+    Console.WriteLine("Введите трехзначное число");    
+    string? str = Convert.ToString(Console.ReadLine());
+    if (str != null)
+    {
+        int number = Convert.ToInt32(str);
+        if ( 99 < number && number < 1000)
+        {
+          
+            Console.Write(str[1]);
+        }
+    }
+        else Console.WriteLine("Вы ввели неверное число");
+}
+}
+}
